de1ee0fa4e180fec118c43b807dbffffb04fe692
RanjitSinghChouhan <90206225+RanjitSinghChouhan@users.noreply.github.com>
2022-01-13 10:48:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const mba = document.getElementById("mba");
+
+function showList() {
+    const arr = ['specialization', 'MBA in Finance', 'MBA in Marketing'];
+    const cont = document.getElementById("container");
+    cont.setAttribute('style', 'display: block; margin-left: 200px; z-index: 1500;')
+    const ul = document.createElement('ul');
+    ul.setAttribute('style', 'padding: 0; margin: 0;');
+    ul.setAttribute('id', 'theList');
+    for (i = 0; i < arr.length; i++) {
+        const li = document.createElement('li');
+        li.innerHTML = arr[i];
+        li.setAttribute('style', 'display: block;');
+        ul.appendChild(li);
+    }
+    cont.appendChild(ul);
+}
+
+function showNothing() {
+    const cont = document.getElementById("container");
+    cont.innerHTML = '';
+}
