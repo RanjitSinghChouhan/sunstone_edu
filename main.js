9f57f17eb45b4196c497bea72991d2b3bd9161c0
RanjitSinghChouhan <90206225+RanjitSinghChouhan@users.noreply.github.com>
2022-01-13 13:48:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const mba = document.getElementById("mba");
 function showList() {
     const arr = ['specialization', 'MBA in Finance', 'MBA in Marketing'];
     const cont = document.getElementById("container");
-    cont.setAttribute('style', 'display: block; margin-left: 200px; z-index: 1500;')
+    cont.setAttribute('style', 'position:absolute; display: block; margin-left: 200px; z-index: 1500;')
     const ul = document.createElement('ul');
     ul.setAttribute('style', 'padding: 0; margin: 0;');
     ul.setAttribute('id', 'theList');
     for (i = 0; i < arr.length; i++) {
         const li = document.createElement('li');
         li.innerHTML = arr[i];
-        li.setAttribute('style', 'display: block;');
+        li.setAttribute('style', 'position:relative; display: block;');
         ul.appendChild(li);
     }
     cont.appendChild(ul);
